8e31f50580112682bef505d483de2f1a59f013fc
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-06-01 11:43:16 +0200

    finalised seeds and cloudinary setup

--- app/models/park.rb
+++ app/models/park.rb
@@ -4,4 +4,5 @@ class Park < ApplicationRecord
   has_many :bookmarks
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many_attached :photos
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,20 +16,20 @@ joe = User.create(email: 'joe@example.com', password: '123456', first_name: 'Joe
 
 park = Park.new(name: "Rålambshovsparken", address: 'Rålambshovsparken', region: 'Kungsholmen')
 file = URI.open('https://upload.wikimedia.org/wikipedia/commons/9/9a/R%C3%A5lambshovsparken_syd_2010.jpg')
-park.photo.attach(io: handle_string_io_as_file(file, 'image.png'), filename: 'park.png', content_type: 'image/png')
+park.photos.attach(io: handle_string_io_as_file(file, 'image.png'), filename: 'park.png', content_type: 'image/png')
 park.save
 
 park_two = Park.new(name: "Tantolunden", address: 'Tantolunden', region: 'Södermalm')
 file = URI.open('https://www.matochresebloggen.se/wp-content/uploads/2015/08/IMG_8178.jpg')
-park.photo.attach(io: handle_string_io_as_file(file, 'image.png'), filename: 'park_two.png', content_type: 'image/png')
+park.photos.attach(io: handle_string_io_as_file(file, 'image.png'), filename: 'park_two.png', content_type: 'image/png')
 park_two.save
 
 park_three = Park.new(name: "Humlegården", address: 'Humlegården', region: 'Östermalm')
 file = URI.open('https://parker.stockholm/siteassets/parker-och-natur/parker/humlegarden/humlegarden-2017.jpg')
-park.photo.attach(io: handle_string_io_as_file(file, 'image.png'), filename: 'park_three.png', content_type: 'image/png')
+park.photos.attach(io: handle_string_io_as_file(file, 'image.png'), filename: 'park_three.png', content_type: 'image/png')
 park_three.save
 
 park_four = Park.new(name: "Vasaparken", address: 'Vasaparken', region: 'Vasastan')
 file = URI.open('http://www.noje.se/uploads/articles/7d51a276bde29a2b7b5f14b661f2df1a.jpg')
-park.photo.attach(io: handle_string_io_as_file(file, 'image.png'), filename: 'park_four.png', content_type: 'image/png')
+park.photos.attach(io: handle_string_io_as_file(file, 'image.png'), filename: 'park_four.png', content_type: 'image/png')
 park_four.save
