721bbc1577db447ae80aba4f25d1dcb2fe91b377
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-06-01 13:03:10 +0200

    show page v1 up and running

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,23 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/dittojoe/ckp5bqvej0xlt18lrqh6k0g52/'
+    });
+    const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach((marker) => {
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .addTo(map);
+          map.flyTo({center: [ marker.lng, marker.lat ], zoom: 12})
+    });
+  }
+};
+
+export { initMapbox };
