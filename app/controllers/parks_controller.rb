f102ad60f800863733957a2e87155940b8a97dcf
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-06-01 12:20:24 +0200

    added migration for details column

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,2 +1,13 @@
 class ParksController < ApplicationController
+  def show
+    @park = Park.find(params[:id])
+    @park_arr = []
+    @park_arr << @park
+    @markers = @park_arr.map do |park|
+      {
+        lat: park.latitude.nil? ? 59.3354662 : park.latitude,
+        lng: park.longitude.nil? ? 18.0600026 : park.longitude
+      }
+    end
+  end
 end
