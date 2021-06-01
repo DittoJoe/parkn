ae1f2bd09500eb04765a45ac25c4eab50f166b79
Cristina Salazar <cristinasalazarm1@gmail.com>
2021-06-01 14:47:48 +0200

    add an end in the park controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -2,7 +2,7 @@ class ParksController < ApplicationController
    def index
     @parks = Park.all
    end
-  
+
   def show
     @park = Park.find(params[:id])
     @park_arr = []
@@ -12,5 +12,6 @@ class ParksController < ApplicationController
         lat: park.latitude.nil? ? 59.3354662 : park.latitude,
         lng: park.longitude.nil? ? 18.0600026 : park.longitude
       }
-   end 
+   end
+ end
 end
