4d6afd3167ae96604fe02656ae0a99b9a608b585
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-06-02 12:35:06 +0200

    added map with markers to index

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -12,12 +18,19 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers);
       markers.forEach((marker) => {
-        new mapboxgl.Marker()
+        const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+        new mapboxgl.Marker({ "color": "#c44536" })
           .setLngLat([ marker.lng, marker.lat ])
+          .setPopup(popup)
           .addTo(map);
-          map.flyTo({center: [ marker.lng, marker.lat ], zoom: 14})
+          if (markers.length > 1) {
+            fitMapToMarkers(map, markers);
+          } else {
+            map.flyTo({center: [ marker.lng, marker.lat ], zoom: 14})
+          }
     });
   }
 };
 
 export { initMapbox };
+
