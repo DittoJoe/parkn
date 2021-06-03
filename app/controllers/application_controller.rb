c4ace2916bc94f2634d67ea2bd2aff558866242d
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-06-03 17:19:35 +0200

    neatened buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :set_counter
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -10,11 +9,4 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
-
-  # private
-
-  # def set_counter
-  #   @park_category = ParkCategory.find(params[:id])
-  #   return @park_category.votes.count
-  # end
 end
