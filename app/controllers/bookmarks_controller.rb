2923ba0a0bef69b9559a373bfc5affab7adb2b29
Mikael Svensson <svensson_0498@hotmail.com>
2021-06-03 17:19:55 +0200

    add a bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,9 @@
 class BookmarksController < ApplicationController
+  def create
+    @bookmark = Bookmark.new
+    @park = Park.find(params[:park_id])
+    @bookmark.park = @park
+    @bookmark.user = current_user
+    @bookmark.save
+  end
 end
