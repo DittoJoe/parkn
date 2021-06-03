d6f1775e643de61e62674005fa8cb1cab037efc2
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-06-03 14:20:50 +0200

    added bookmarks routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :parks, except: :destroy
+  resources :parks, except: :destroy do
+    resources :bookmarks, only: :create
+  end
+  resources :bookmarks, only: [:index, :destroy]
   resources :park_categories do
     resources :votes, only: [:create, :index]
   end
