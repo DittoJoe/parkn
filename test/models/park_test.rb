bc854fce8d00da5b0d94831d1ea5607e33a1b1fd
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-08-09 19:58:25 +0200

    added a basic test to ensure a park cannot be created without name

--- test/models/park_test.rb
+++ test/models/park_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ParkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a park without a name" do
+    park = Park.new
+    assert_not park.save, "Park was saved without a title"
+  end
+
+  test "should not save a park without a region" do
+    park = Park.new(name: 'testpark')
+    assert_not park.save, "Park was saved without a region"
+  end
 end
+
