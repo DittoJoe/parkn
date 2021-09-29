8f436030a25a164a3fa0f5f5425d6525fc897b0e
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-09-29 15:02:18 +0200

    fixed an issue where the map landed on france if there were no search results

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,20 +14,22 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/dittojoe/ckp5bqvej0xlt18lrqh6k0g52/'
+      style: 'mapbox://styles/dittojoe/ckp5bqvej0xlt18lrqh6k0g52/',
+      center: [18.0600026, 59.3354662],
+      zoom: 11.15
     });
     const markers = JSON.parse(mapElement.dataset.markers);
-      markers.forEach((marker) => {
-        const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-        new mapboxgl.Marker({ "color": "#c44536" })
-          .setLngLat([ marker.lng, marker.lat ])
-          .setPopup(popup)
-          .addTo(map);
-          if (markers.length > 1) {
-            fitMapToMarkers(map, markers);
-          } else {
-            map.flyTo({center: [ marker.lng, marker.lat ], zoom: 14})
-          }
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+      new mapboxgl.Marker({ "color": "#c44536" })
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+      if (markers.length > 1) {
+        fitMapToMarkers(map, markers);
+      } else if (markers.length == 1) {
+        map.flyTo({center: [ marker.lng, marker.lat ], zoom: 14})
+      }
     });
   }
 };
